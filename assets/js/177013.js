a369a1008a7580c47761259857b6547cc7faafcb
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2025-06-10 00:06:11 +0800

    langswitch

--- assets/js/177013.js
+++ assets/js/177013.js
@@ -8,3 +8,22 @@ function startAudio() {
   document.getElementById("playAud").play();
   document.title = preStartAudio();
 }
+
+function langQue() {
+  let lang = new URLSearchParams(window.location.search).get("lang"),
+    txt = document.getElementById("centerText");
+  switch (lang) {
+    case "ja":
+      txt.innerHTML = `
+        <div lang="ja">
+          <p>もうすぐ、また私の物語を皆が聞くことになるでしょう。</p>
+          <br>
+          <p>吉田咲🌹</p>
+        </div>
+      `;
+      return;
+    case "en":
+    default:
+      return;
+  }
+}
